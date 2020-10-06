6d942b74c021598494760547d9cdecbf329112ae
Odair Pedro <brhbs0455@ambev.com.br>
2020-10-06 13:01:54 -0300

    Install script

---
+++ install-local.sh
@@ -0,0 +1,19 @@
+printBanner() {
+    echo " ____                            ____ ___ "
+    echo "/ ___|  ___  _ __   __ _ _ __   / ___|_ _|"
+    echo "\___ \ / _ \| '_ \ / _  | '__| | |    | | "
+    echo " ___) | (_) | | | | (_| | |    | |___ | | "
+    echo "|____/ \___/|_| |_|\__,_|_|     \____|___|"
+    echo ""
+    echo "has been installed with success"
+}
+
+install() {
+    wget https://github.com/odair-pedro/sonarci/releases/latest/download/sonarci-linux-x64.tar.gz
+    tar -xf sonarci-linux-x64.tar.gz sonarci
+    rm sonarci-linux-x64.tar.gz
+    
+    printBanner
+}
+
+install
