7f777f98c3f0063b3e7ba0f14b7b3e5cc7e0d6a6
Odair Pedro <brhbs0455@ambev.com.br>
2020-10-20 20:37:19 -0300

    Implementing quality gate validation

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"log"
 	"sonarci/cmd"
-	"sonarci/logging"
+	"sonarci/net/http"
+	"sonarci/sonar/rest/base"
+	"time"
 )
 
 var version string
 
 func main() {
-	logging.Setup()
-	startRootCommand()
+	//logging.Setup()
+	//startRootCommand()
+
+	api := base.NewRestApi(http.NewConnection("https://sonarqube.ambevdevs.com.br", "00500edbf41ebdc63f48646a5e86562fcc0b9692", time.Second*30))
+	result, _ := api.GetPullRequestQualityGate("ambev-franqueado-gaming-server", "23195")
+	_ = result
 }
 
 func startRootCommand() {
